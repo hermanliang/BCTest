ff3dd430fcab20e27d587809f82adc69ae09b3af
Herman Liang <herman@kaiwood.com.tw>
2013-01-11 09:40:07 +0800

    Change target resolution
    Signed-off-by: Herman Liang <herman@kaiwood.com.tw>

--- KwBarcode/KwQRCodeReader.cs
+++ KwBarcode/KwQRCodeReader.cs
@@ -156,8 +156,8 @@ namespace KwBarcode
                 image = new Bitmap(
                     image,
                     new Size(
-                        (int)((double)image.Width / minLen * 512),
-                        (int)((double)image.Height / minLen * 512)
+                        (int)((double)image.Width / minLen * 256),
+                        (int)((double)image.Height / minLen * 256)
                     )
                 );
             //}
